113ebd0ca4defd896a7ae16e33d5112c06b91b61
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-18 18:28:34 +0200

    Moved BlockLanguageSwitch

---
+++ src/Blocks/LanguageSwitch.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Language\Blocks;
+
+use VitesseCms\Block\AbstractBlockModel;
+use VitesseCms\Block\Models\Block;
+use VitesseCms\Language\Models\Language;
+
+class LanguageSwitch extends AbstractBlockModel
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->excludeFromCache = true;
+    }
+
+    public function parse(Block $block): void
+    {
+        parent::parse($block);
+        $languages = Language::find();
+
+        if ($this->view->hasCurrentItem()) :
+            $currentItem = $this->view->getCurrentItem();
+            /** @var Language $language */
+            foreach ($languages as $key => $language):
+                $language->set('slug', $currentItem->_('slug', $language->_('short')));
+                $language->set('showDelimiter', true);
+                if ($language->_('slug') === '/') :
+                    $language->set('slug', null);
+                    $language->set('showDelimiter', false);
+                elseif (substr_count($language->_('domain'), '/' . $language->getShortCode() . '/')) :
+                    $language->set('showDelimiter', false);
+                endif;
+            endforeach;
+        endif;
+
+        $this->view->set('hrefLanguages', $languages);
+        $block->set('languages', $languages);
+    }
+}
