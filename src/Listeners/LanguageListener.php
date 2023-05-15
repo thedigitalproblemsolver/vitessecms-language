f49c4356824109105801cd519c0d91032ca488a8
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-05-15 23:21:48 +0200

    Added Editable Added Deletable

--- src/Listeners/LanguageListener.php
+++ src/Listeners/LanguageListener.php
@@ -3,15 +3,24 @@
 namespace VitesseCms\Language\Listeners;
 
 use VitesseCms\Language\Repositories\LanguageRepository;
+use VitesseCms\Language\Services\LanguageService;
 
 class LanguageListener
 {
-    public function __construct(private readonly LanguageRepository $languageRepository)
+    public function __construct(
+        private readonly LanguageRepository $languageRepository,
+        private readonly LanguageService    $languageService
+    )
     {
     }
 
     public function getRepository(): LanguageRepository
     {
         return $this->languageRepository;
+    }
+
+    public function attach(): LanguageService
+    {
+        return $this->languageService;
     }
 }
